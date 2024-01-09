839e9eb3fec9928fcf100454a2a2bdc1e89160db
wills_dev <godswillijezei@gmail.com>
2024-01-09 16:32:16 +0100

    routing

--- src/lib/components/admin/category/AddCategory.tsx
+++ src/lib/components/admin/category/AddCategory.tsx
@@ -1,6 +1,57 @@
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import TextInput, { InputType } from "../../ui/TextInput";
+import Button from "../../ui/Button";
+import { ScaleSpinner } from "../../ui/Loading";
+
 const AddCategory = () => {
+  const [isBusy, setIsBusy] = useState(false)
+    const {
+        control,
+        handleSubmit,
+        formState: { errors, isValid },
+      } = useForm({
+        mode: "onChange",
+        defaultValues: {
+          email: "",
+        },
+      });
+    const onSubmit = () => {
+        setIsBusy(true)
+    }
   return (
-    <div>AddCategory</div>
+    <>
+      <div>
+    <form onSubmit={handleSubmit(onSubmit)} >
+      <div>
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: "Please enter your email",
+            },
+          }}
+          render={({ field }) => (
+            <TextInput
+              label="Email"
+              labelClassName="text-[#000000B2] fw-500"
+              placeholder="victorchigozie@gmail.com"
+              error={errors.email?.message}
+              type={InputType.email}
+              {...field}
+              ref={null}
+            />
+          )}
+        />
+      </div>
+      <div className="mt-12">
+        <Button title={isBusy ? <ScaleSpinner size={14} color="white"/> : "Continue"} disabled={!isValid} />
+      </div>
+    </form>
+  </div>
+    </>
   )
 }
 
