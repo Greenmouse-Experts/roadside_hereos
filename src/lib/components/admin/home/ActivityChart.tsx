53456c48cc8667fec006b4b9a4484cac4b7bbb82
wills <godswillIjezei@gmail.com>
2024-05-21 10:46:47 +0100

    payments

--- src/lib/components/admin/home/ActivityChart.tsx
+++ src/lib/components/admin/home/ActivityChart.tsx
@@ -12,86 +12,86 @@ import {
 // const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 
-  const series = [
-    {
-      name: "Sales",
-      data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
-    },
-  ]
-  const options = {
-    chart: {
-      toolbar: {
-        show: false,
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    colors: ["#020617"],
-    plotOptions: {
-      bar: {
-        columnWidth: "40%",
-        borderRadius: 2,
-      },
-    },
-    xaxis: {
-      axisTicks: {
-        show: false,
-      },
-      axisBorder: {
-        show: false,
-      },
-      labels: {
-        style: {
-          colors: "#616161",
-          fontSize: "12px",
-          fontFamily: "inherit",
-          fontWeight: 400,
-        },
-      },
-      categories: [
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ],
-    },
-    yaxis: {
-      labels: {
-        style: {
-          colors: "#616161",
-          fontSize: "12px",
-          fontFamily: "inherit",
-          fontWeight: 400,
-        },
-      },
-    },
-    grid: {
-      show: true,
-      borderColor: "#dddddd",
-      strokeDashArray: 5,
-      xaxis: {
-        lines: {
-          show: true,
-        },
-      },
-      padding: {
-        top: 5,
-        right: 20,
-      },
-    },
-    fill: {
-      opacity: 0.8,
-    },
-    tooltip: {
-      theme: "dark",
-    },
-  }
+  // const series = [
+  //   {
+  //     name: "Sales",
+  //     data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+  //   },
+  // ]
+  // const options = {
+  //   chart: {
+  //     toolbar: {
+  //       show: false,
+  //     },
+  //   },
+  //   dataLabels: {
+  //     enabled: false,
+  //   },
+  //   colors: ["#020617"],
+  //   plotOptions: {
+  //     bar: {
+  //       columnWidth: "40%",
+  //       borderRadius: 2,
+  //     },
+  //   },
+  //   xaxis: {
+  //     axisTicks: {
+  //       show: false,
+  //     },
+  //     axisBorder: {
+  //       show: false,
+  //     },
+  //     labels: {
+  //       style: {
+  //         colors: "#616161",
+  //         fontSize: "12px",
+  //         fontFamily: "inherit",
+  //         fontWeight: 400,
+  //       },
+  //     },
+  //     categories: [
+  //       "Apr",
+  //       "May",
+  //       "Jun",
+  //       "Jul",
+  //       "Aug",
+  //       "Sep",
+  //       "Oct",
+  //       "Nov",
+  //       "Dec",
+  //     ],
+  //   },
+  //   yaxis: {
+  //     labels: {
+  //       style: {
+  //         colors: "#616161",
+  //         fontSize: "12px",
+  //         fontFamily: "inherit",
+  //         fontWeight: 400,
+  //       },
+  //     },
+  //   },
+  //   grid: {
+  //     show: true,
+  //     borderColor: "#dddddd",
+  //     strokeDashArray: 5,
+  //     xaxis: {
+  //       lines: {
+  //         show: true,
+  //       },
+  //     },
+  //     padding: {
+  //       top: 5,
+  //       right: 20,
+  //     },
+  //   },
+  //   fill: {
+  //     opacity: 0.8,
+  //   },
+  //   tooltip: {
+  //     theme: "dark",
+  //   },
+  // }
 
 const ActivityChart = () => {
   return (
