d4fddc9d75c732f3b1ea25690d92c414a6be1c77
princeemeka965 <anyanwue4@gmail.com>
2024-12-03 12:39:51 +0100

    fix suspend and unsuspend issue

--- src/lib/components/admin/providers/details/CompanyProviders.tsx
+++ src/lib/components/admin/providers/details/CompanyProviders.tsx
@@ -1,9 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-import { FC } from "react";
-import { getCompanyProviders } from "../../../../services/api/usersApi";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { FC, useState } from "react";
+import {
+  getCompanyProviders,
+  unsuspendUser,
+} from "../../../../services/api/usersApi";
 import StaffList from "./StaffList";
 import EmptyState from "../../../ui/EmptyState";
 import CurveLoader from "../../../ui/loader/curveLoader/CurveLoader";
+import useModal from "../../../../hooks/useModal";
+import SuspendModal from "./SuspendModal";
+import ReusableModal from "../../../ui/ReusableModal";
+import { toast } from "react-toastify";
 
 interface Props {
   id: string;
@@ -13,6 +20,49 @@ const CompanyProviders: FC<Props> = ({ id }) => {
     queryFn: () => getCompanyProviders(id),
     queryKey: ["companyProviders"],
   });
+
+  const [isBusy, setIsBusy] = useState(false);
+  const { Modal: Suspend, setShowModal: ShowSuspend } = useModal();
+  const { Modal: Unsuspend, setShowModal: ShowUnsuspend } = useModal();
+  const [dataId, setId] = useState("");
+
+  const unsus = useMutation({
+    mutationFn: unsuspendUser,
+    mutationKey: ["unsuspend"],
+  });
+
+  const UnsuspendAction = () => {
+    const payload = {
+      userId: dataId,
+    };
+    unsus.mutate(payload, {
+      onSuccess: (data) => {
+        toast.success(data.message);
+        setIsBusy(false);
+        refetch();
+        ShowUnsuspend(false);
+      },
+      onError: (error) => {
+        toast.error(error.message);
+        setIsBusy(false);
+      },
+    });
+  };
+
+  let getId = "";
+
+  const handleSuspendModal = () => {
+    ShowSuspend(true);
+    getId = localStorage.getItem("providerId") || "";
+    setId(getId);
+  };
+
+  const handleUnSuspendModal = () => {
+    ShowUnsuspend(true);
+    getId = localStorage.getItem("providerId") || "";
+    setId(getId);
+  };
+
   return (
     <>
       <div className="bg-white p-6 shadow rounded min-h-[350px]">
@@ -33,7 +83,32 @@ const CompanyProviders: FC<Props> = ({ id }) => {
             <EmptyState msg="There are no providers currently in this company." />
           </div>
         )}
-        {data && <StaffList data={data?.data} refetch={refetch} />}
+        {data && (
+          <StaffList
+            data={data?.data}
+            setSuspend={() => handleSuspendModal()}
+            setUnSuspend={() => handleUnSuspendModal()}
+          />
+        )}
+
+        <Suspend title="Suspend Driver" size="sm" type="withCancel">
+          <SuspendModal
+            id={dataId}
+            close={() => ShowSuspend(false)}
+            refetch={refetch}
+          />
+        </Suspend>
+
+        <Unsuspend title="" size="sm">
+          <ReusableModal
+            title="Do you want to unsuspend this driver"
+            action={UnsuspendAction}
+            closeModal={() => ShowUnsuspend(false)}
+            actionTitle="Unsuspend"
+            cancelTitle="Close"
+            isBusy={isBusy}
+          />
+        </Unsuspend>
       </div>
     </>
   );
--- src/lib/components/admin/providers/details/StaffList.tsx
+++ src/lib/components/admin/providers/details/StaffList.tsx
@@ -13,54 +13,33 @@ import { BsArrowsExpand, BsThreeDotsVertical } from "react-icons/bs";
 // import { FormatStatus } from "../../../utils";
 import { useNavigate } from "react-router-dom";
 // import { GetInvitedItem, StaffItem } from "../../../types/company";
-import { FC, useState } from "react";
+import { FC } from "react";
 import { StaffItem } from "../../../../types/company";
 import ProfileAvatar from "../../../ui/ProfileAvatar";
 import { FormatStatus } from "../../../../utils";
 import { DataTable } from "../../../ui/DataTable";
-import SuspendModal from "./SuspendModal";
-import useModal from "../../../../hooks/useModal";
-import ReusableModal from "../../../ui/ReusableModal";
-import { useMutation } from "@tanstack/react-query";
-import { unsuspendUser } from "../../../../services/api/usersApi";
-import { toast } from "react-toastify";
 
 interface Props {
   data: any;
-  refetch: () => void;
+  setSuspend: any;
+  setUnSuspend: any;
 }
-const StaffList: FC<Props> = ({ data, refetch }) => {
-  const [isBusy, setIsBusy] = useState(false);
-  const { Modal: Suspend, setShowModal: ShowSuspend } = useModal();
-  const { Modal: Unsuspend, setShowModal: ShowUnsuspend } = useModal();
-
-  const unsus = useMutation({
-    mutationFn: unsuspendUser,
-    mutationKey: ["unsuspend"],
-  });
-
-  const UnsuspendAction = () => {
-    const payload = {
-      userId: data[0].id,
-    };
-    unsus.mutate(payload, {
-      onSuccess: (data) => {
-        toast.success(data.message);
-        setIsBusy(false);
-        refetch();
-        ShowUnsuspend(false);
-      },
-      onError: (error) => {
-        toast.error(error.message);
-        setIsBusy(false);
-      },
-    });
-  };
-
+const StaffList: FC<Props> = ({ data, setSuspend, setUnSuspend }) => {
   const navigate = useNavigate();
   const gotoDetails = (item: string) => {
     navigate(`/admin/providers/staff/${item}`);
   };
+
+  const handleSuspend = (id: any) => {
+    localStorage.setItem("providerId", id);
+    setSuspend(true);
+  };
+
+  const handleUnSuspend = (id: any) => {
+    localStorage.setItem("providerId", id);
+    setUnSuspend(true);
+  };
+
   // Table components
   const columnHelper = createColumnHelper<StaffItem>();
   const columns = [
@@ -122,43 +101,24 @@ const StaffList: FC<Props> = ({ data, refetch }) => {
               >
                 <BsArrowsExpand /> View Details
               </MenuItem>
-              {!data[0].isSuspended && (
+              {!info.row.original.isSuspended && (
                 <MenuItem
                   className="my-1 fw-500 flex items-center gap-x-2 pt-1"
-                  onClick={() => ShowSuspend(true)}
+                  onClick={() => handleSuspend(info.getValue())}
                 >
                   Suspend
                 </MenuItem>
               )}
-              {data[0].isSuspended && (
+              {info.row.original.isSuspended && (
                 <MenuItem
                   className="my-1 fw-500 flex items-center gap-x-2 pt-1"
-                  onClick={() => ShowUnsuspend(true)}
+                  onClick={() => handleUnSuspend(info.getValue())}
                 >
                   UnSuspend
                 </MenuItem>
               )}
             </MenuList>
           </Menu>
-
-          <Suspend title="Suspend Driver" size="sm" type="withCancel">
-            <SuspendModal
-              id={data[0].id}
-              close={() => ShowSuspend(false)}
-              refetch={refetch}
-            />
-          </Suspend>
-
-          <Unsuspend title="" size="sm">
-            <ReusableModal
-              title="Do you want to unsuspend this driver"
-              action={UnsuspendAction}
-              closeModal={() => ShowUnsuspend(false)}
-              actionTitle="Unsuspend"
-              cancelTitle="Close"
-              isBusy={isBusy}
-            />
-          </Unsuspend>
         </>
       ),
     }),
