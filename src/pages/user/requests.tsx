3d98a39120343d4d61d77dafe8980e5b3d07c492
destiny <desto4q@gmail.com>
2025-09-12 16:33:25 +0100

    fulfilled

--- src/pages/user/requests.tsx
+++ src/pages/user/requests.tsx
@@ -1,33 +1,32 @@
-import Tabs from "../../lib/components/ui/Tabs"
-import RequestList from "../../lib/components/user/requests/RequestList"
+import Tabs from "../../lib/components/ui/Tabs";
+import RequestList from "../../lib/components/user/requests/RequestList";
 
 const UserRequests = () => {
-   
-    const tabs = [
-        {
-            title: <p>Processing</p>,
-            content: <RequestList status="Pending" paymentStatus="Paid"/>
-        },
-        {
-            title: <p>Ongoing</p>,
-            content: <RequestList status="Ongoing" paymentStatus="Paid"/>
-        },
-        {
-            title: <p>Fulfilled</p>,
-            content: <RequestList status="fulfilled" paymentStatus="Pending"/>
-        },
-        {
-            title: <p>Completed</p>,
-            content: <RequestList status="completed" paymentStatus="Paid"/>
-        },
-    ]
+  const tabs = [
+    {
+      title: <p>Processing</p>,
+      content: <RequestList status="Pending" paymentStatus="Paid" />,
+    },
+    {
+      title: <p>Ongoing</p>,
+      content: <RequestList status="Ongoing" paymentStatus="Paid" />,
+    },
+    {
+      title: <p>Fulfilled</p>,
+      content: <RequestList status="fulfilled" paymentStatus="Paid" />,
+    },
+    {
+      title: <p>Completed</p>,
+      content: <RequestList status="completed" paymentStatus="Paid" />,
+    },
+  ];
   return (
     <>
-        <div className='bg-white p-2 lg:p-6 rounded-lg shadow min-h-[80vh]'>
-            <Tabs tabs={tabs} type='charts'/>
-        </div>
+      <div className="bg-white p-2 lg:p-6 rounded-lg shadow min-h-[80vh]">
+        <Tabs tabs={tabs} type="charts" />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default UserRequests
+export default UserRequests;
