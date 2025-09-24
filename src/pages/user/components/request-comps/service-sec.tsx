7972909f328ddf30af1eed53326ba73e90f1820d
destiny <desto4q@gmail.com>
2025-09-24 20:43:20 +0100

    zip code removed from service

--- src/pages/user/components/request-comps/service-sec.tsx
+++ src/pages/user/components/request-comps/service-sec.tsx
@@ -268,7 +268,7 @@ export default function ServiceSection() {
           }}
         />
       </div>
-      <div className="col-span-2 ">
+      {/*<div className="col-span-2 ">
         <label className="mb-2 block mt-2 fw-600 text-[#000000B2]">
           Zip Code
         </label>
@@ -276,7 +276,7 @@ export default function ServiceSection() {
           {...register("zipcode")}
           className="border border-gray-400 w-full mt-[4px] p-[9px] rounded"
         />
-      </div>
+      </div>*/}
       <div className="mb-2 col-span-2 mt-2">
         <label className="mb-1 block mt-2 fw-600 text-[#000000B2]">
           Request Note (required)
