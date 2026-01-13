3c06df6218fac998bb760675c7c834848a778ea6
destiny <desto4q@gmail.com>
2026-01-13 08:23:57 +0100

    social lnks

--- src/lib/components/layout/landing/sections/Footer.tsx
+++ src/lib/components/layout/landing/sections/Footer.tsx
@@ -77,7 +77,9 @@ const Footer = () => {
               <ul className="flex gap-x-5 lg:gap-x-4 mt-8">
                 {socials.map((item, i) => (
                   <li className="text-white cursor-pointer" key={i}>
-                    <Link to={item.to}>{item.icon}</Link>
+                    <a href={item.to} target="_blank" rel="noopener noreferrer">
+                      {item.icon}
+                    </a>
                   </li>
                 ))}
               </ul>
